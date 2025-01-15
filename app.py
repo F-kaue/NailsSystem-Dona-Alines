e925f90c9f47a71015cc54a4e49ed75e84c5f9de
gisele <desenvolvedor@sindsystem.com.br>
2025-01-15 12:37:02 -0300

    Adicionando Todos os arquivos

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for, session
+import os
 
 app = Flask(__name__)
 app.secret_key = "chave_secreta"  # Necessária para usar sessões
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Pega a porta do ambiente ou usa 5000
+    app.run(host="0.0.0.0", port=port)
+
 
 # Lista de barbeiros simulando uma base de dados
 barbeiros = [
